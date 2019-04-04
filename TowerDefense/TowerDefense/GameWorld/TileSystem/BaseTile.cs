43bb7dab134a94f4860568e0eaef6ffa22f927f1
Daan Stout <formaestroke@gmail.com>
2019-04-04 14:51:47 +0100

    Update BaseTile.cs

--- TowerDefense/TowerDefense/GameWorld/TileSystem/BaseTile.cs
+++ TowerDefense/TowerDefense/GameWorld/TileSystem/BaseTile.cs
@@ -11,6 +11,7 @@ namespace TowerDefense.GameWorld.TileSystem {
     /// A base tile that occupies the world
     /// </summary>
     public abstract class BaseTile {
+        #region Constants
         /// <summary>
         /// The width of a tile
         /// </summary>
@@ -19,7 +20,8 @@ namespace TowerDefense.GameWorld.TileSystem {
         /// The height of a tile
         /// </summary>
         public const int TILE_HEIGHT = 25;
-
+        #endregion
+        #region Variables
         /// <summary>
         /// The sprite of the tile
         /// </summary>
@@ -32,6 +34,58 @@ namespace TowerDefense.GameWorld.TileSystem {
         /// The position of the tile
         /// </summary>
         public Vector2D position;
-        
+        /// <summary>
+        /// Indicates whether this tile can be walked over
+        /// </summary>
+        public bool isWalkable;
+        #endregion
+        #region Properties
+        /// <summary>
+        /// Gets a rectangle representing this tile
+        /// </summary>
+        public Rectangle tileRectangle => new Rectangle(position, new Vector2D(TILE_WIDTH, TILE_HEIGHT));
+        #endregion
+        #region Constructors
+        /// <summary>
+        /// Instantiates a new BaseTile
+        /// </summary>
+        /// <param name="position">The position of the tile</param>
+        protected BaseTile(Vector2D position) : this(position, Color.Black) { }
+
+        protected BaseTile(Vector2D position, Bitmap sprite) : this(position, Color.Black, sprite) { }
+
+        /// <summary>
+        /// Instantiates a new BaseTile
+        /// </summary>
+        /// <param name="position">The position of the tile</param>
+        /// <param name="tileColor">The color of the tile</param>
+        protected BaseTile(Vector2D position, Color tileColor) : this(position, tileColor, false) { }
+
+        protected BaseTile(Vector2D position, Color tileColor, Bitmap sprite) : this(position, tileColor, false, sprite) { }
+
+        /// <summary>
+        /// Instantiates a new BaseTile
+        /// </summary>
+        /// <param name="position">The position of the tile</param>
+        /// <param name="isWalkable">Whether the tile can be walked over</param>
+        protected BaseTile(Vector2D position, bool isWalkable) : this(position, Color.Black, isWalkable) { }
+
+        protected BaseTile(Vector2D position, bool isWalkable, Bitmap sprite) : this(position, Color.Black, isWalkable, sprite) { }
+
+        /// <summary>
+        /// Instantiates a new BaseTile
+        /// </summary>
+        /// <param name="position">The position of the tile</param>
+        /// <param name="tileColor">The color of the tile</param>
+        /// <param name="isWalkable">Whether the tile can be walked over</param>
+        protected BaseTile(Vector2D position, Color tileColor, bool isWalkable) : this(position, tileColor, isWalkable, null) { }
+
+        protected BaseTile(Vector2D position, Color tileColor, bool isWalkable, Bitmap sprite) {
+            this.position = position;
+            this.tileColor = tileColor;
+            this.isWalkable = isWalkable;
+            this.sprite = sprite;
+        }
+        #endregion
     }
 }
