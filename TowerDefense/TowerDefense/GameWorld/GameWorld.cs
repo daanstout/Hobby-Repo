5534cc1e423dad722c53586ffeabf67530d5ec45
Daan Stout <formaestroke@gmail.com>
2019-04-08 14:53:38 +0100

    added some tile shit

--- TowerDefense/TowerDefense/GameWorld/GameWorld.cs
+++ TowerDefense/TowerDefense/GameWorld/GameWorld.cs
@@ -5,11 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 using DaanLib.StateMachine;
+using TowerDefense.GameWorld.Tiles;
 
 namespace TowerDefense.GameWorld {
     public class GameWorld {
-        public GameWorld() {
-            
+        public static GameWorld instance;
+
+        private TileSystem tileSystem;
+
+        public readonly int gameWidth;
+        public readonly int gameHeight;
+
+        public GameWorld(int width, int height) {
+            instance = this;
+
+            gameWidth = width;
+            gameHeight = height;
+
+            tileSystem = new TileSystem();
+
+            tileSystem.InitTileSystem();
         }
     }
 }
