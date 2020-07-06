648184ea59674930bb660bd15299752542a51f81
Daan Stout <formaestroke@gmail.com>
2020-07-06 14:04:06 +0200

    removed projects
    removed empty or underdeveloped projects

--- VirtualMachine/VirtualMachine/Extensions/DictionaryExtensionMethods.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace VirtualMachine.Extensions.DictionaryExtensionMethods {
-    public static class DictionaryExtensionMethods {
-        public static ushort Get16(this Dictionary<TKey, string>)
-    }
-}
